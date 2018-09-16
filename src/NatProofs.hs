2ece537a22dd074dad5d4c174ba2b9e247d78d3d
Alexandros Peitsinis <alexpeitsinis@gmail.com>
2018-09-17 00:16:11 +0100

    Made proofs readable

--- src/NatProofs.hs
+++ src/NatProofs.hs
@@ -83,30 +83,31 @@ plusComm a      SZ      = gcastWith (plusIdenL a) Refl
 plusComm SZ     (SS SZ) = Refl
 plusComm (SS a) (SS SZ) = gcastWith (plusComm a (SS SZ)) Refl
 plusComm a      k@(SS b)  =
-  let
-    x1 :: SNat a -> SNat (S b) -> (a + S b) :~: (a + (b + I))
-    x1 _ b' = gcastWith (given2 b' (SS SZ)) Refl
+  let proof :: forall a b. SNat a -> SNat b -> (a + S b) :~: (S b + a)
+      proof x y = x1 ==> x2 ==> x3 ==> x4 ==> x5 ==> x6 ==> x7
+        where
+          x1 :: (a + S b) :~: (a + (b + I))
+          x1 = gcastWith (given2 y (SS SZ)) Refl
 
-    x2 :: SNat a -> SNat b -> (a + (b + I)) :~: ((a + b) + I)
-    x2 a' b' = gcastWith (plusAssoc a' b' (SS SZ)) Refl
+          x2 :: (a + (b + I)) :~: ((a + b) + I)
+          x2 = gcastWith (plusAssoc x y (SS SZ)) Refl
 
-    x3 :: SNat a -> SNat b -> ((a + b) + I) :~: ((b + a) + I)
-    x3 a' b' = gcastWith (plusComm a' b') Refl
+          x3 :: ((a + b) + I) :~: ((b + a) + I)
+          x3 = gcastWith (plusComm x y) Refl
 
-    x4 :: SNat a -> SNat b -> ((b + a) + I) :~: (b + (a + I))
-    x4 a' b' = gcastWith (plusAssoc b' a' (SS SZ)) Refl
+          x4 :: ((b + a) + I) :~: (b + (a + I))
+          x4 = gcastWith (plusAssoc y x (SS SZ)) Refl
 
-    x5 :: SNat a -> SNat b -> (b + (a + I)) :~: (b + (I + a))
-    x5 a' _ = gcastWith (plusComm a' (SS SZ)) Refl
+          x5 :: (b + (a + I)) :~: (b + (I + a))
+          x5 = gcastWith (plusComm x (SS SZ)) Refl
 
-    x6 :: SNat a -> SNat b -> (b + (I + a)) :~: ((b + I) + a)
-    x6 a' b' = gcastWith (plusAssoc b' (SS SZ) a') Refl
+          x6 :: (b + (I + a)) :~: ((b + I) + a)
+          x6 = gcastWith (plusAssoc y (SS SZ) x) Refl
 
-    x7 :: SNat a -> SNat b -> ((b + I) + a) :~: (S b + a)
-    x7 _ b' = gcastWith (given2 b' (SS SZ)) Refl
+          x7 :: ((b + I) + a) :~: (S b + a)
+          x7 = gcastWith (given2 y (SS SZ)) Refl
 
-  in x1 a k ==> x2 a b ==> x3 a b ==> x4 a b ==> x5 a b ==> x6 a b ==> x7 a b
-
+  in proof a b
 
 --
 -- Multiplication
@@ -146,199 +147,84 @@ mulIdenL :: SNat a -> (I * a) :~: a
 mulIdenL SZ     = Refl
 mulIdenL (SS a) = gcastWith (mulIdenL a) Refl
 
-mulPlusDistHelper ::
-  forall a b c.
-  (
-    IsNat a, IsNat b, IsNat c
-  , IsNat (a * c), IsNat (b * c), IsNat (a + b)
-  , IsNat ((a * c) + a)
-  ) =>
-  SNat a -> SNat b -> SNat c ->
-  (((a + b) * c) + (a + b)) :~: ((a * S c) + (b * S c))
-mulPlusDistHelper a b c =
-  let
-    x1 :: (((a + b) * c) + (a + b)) :~: (((a * c) + (b * c)) + (a + b))
-    x1 = gcastWith (mulPlusDist (nat @a) (nat @b) (nat @c)) Refl
-
-    x2 :: (((a * c) + (b * c)) + (a + b)) :~: ((a * c) + ((b * c) + (a + b)))
-    x2 = gcastWith (plusAssoc (nat @(a * c)) (nat @(b * c)) (nat @(a + b))) Refl
-
-    x3 :: ((a * c) + ((b * c) + (a + b))) :~: ((a * c) + ((a + b) + (b * c)))
-    x3 = gcastWith (plusComm (nat @(b * c)) (nat @(a + b))) Refl
-
-    x4 :: ((a * c) + ((a + b) + (b * c))) :~: (((a * c) + (a + b)) + (b * c))
-    x4 = gcastWith (plusAssoc (nat @(a * c)) (nat @(a + b)) (nat @(b * c))) Refl
-
-    x5 :: (((a * c) + (a + b)) + (b * c)) :~: ((((a * c) + a) + b) + (b * c))
-    x5 = gcastWith (plusAssoc (nat @(a * c)) (nat @a) (nat @b)) Refl
-
-    x6 :: ((((a * c) + a) + b) + (b * c)) :~: (((a * c) + a) + (b + (b * c)))
-    x6 = gcastWith (plusAssoc (nat @((a * c) + a)) (nat @b) (nat @(b * c))) Refl
-
-    x7 :: (((a * c) + a) + (b + (b * c))) :~: (((a * c) + a) + ((b * c) + b))
-    x7 = gcastWith (plusComm (nat @b) (nat @(b * c))) Refl
-
-    x8 :: (((a * c) + a) + ((b * c) + b)) :~: ((a * S c) + (b * S c))
-    x8 = Refl
-
-  in x1 ==> x2 ==> x3 ==> x4 ==> x5 ==> x6 ==> x7 ==> x8
-
 -- Proof that multiplication distributes over addition
 mulPlusDist :: SNat a -> SNat b -> SNat c -> ((a + b) * c) :~: ((a * c) + (b * c))
 mulPlusDist a b SZ       = Refl
 mulPlusDist a b k@(SS c) =
-  let
-    x1 :: SNat a -> SNat b -> SNat (S c) -> ((a + b) * S c) :~: (((a + b) * c) + (a + b))
-    x1 _ _ _ = Refl -- from (4)
+  let proof :: forall a b c. SNat a -> SNat b -> SNat c -> ((a + b) * S c) :~: ((a * S c) + (b * S c))
+      proof x y z = x1 ==> x2 ==> x3 ==> x4 ==> x5 ==> x6 ==> x7 ==> x8 ==> x9
+        where
+          x1 :: ((a + b) * S c) :~: (((a + b) * c) + (a + b))
+          x1 = Refl -- from (4)
 
-    x2 :: SNat a -> SNat b -> SNat c -> (((a + b) * c) + (a + b)) :~: (((a * c) + (b * c)) + (a + b))
-    x2 a' b' c' = gcastWith (mulPlusDist a' b' c') Refl
+          x2 :: (((a + b) * c) + (a + b)) :~: (((a * c) + (b * c)) + (a + b))
+          x2 = gcastWith (mulPlusDist x y z) Refl
 
-    x3 ::
-      SNat a -> SNat b -> SNat c ->
-      SNat (a * c) -> SNat (b * c) -> SNat (a + b) ->
-      (((a * c) + (b * c)) + (a + b)) :~: ((a * c) + ((b * c) + (a + b)))
-    x3 _ _ _ x y z = gcastWith (plusAssoc x y z) Refl
+          x3 :: (((a * c) + (b * c)) + (a + b)) :~: ((a * c) + ((b * c) + (a + b)))
+          x3 = gcastWith (plusAssoc xmz ymz xpy) Refl
 
-    x4 ::
-      SNat a -> SNat b -> SNat c ->
-      SNat (b * c) -> SNat (a + b) ->
-      ((a * c) + ((b * c) + (a + b))) :~: ((a * c) + ((a + b) + (b * c)))
-    x4 _ _ _ x y = gcastWith (plusComm x y) Refl
+          x4 :: ((a * c) + ((b * c) + (a + b))) :~: ((a * c) + ((a + b) + (b * c)))
+          x4 = gcastWith (plusComm ymz xpy) Refl
 
-    x5 ::
-      SNat a -> SNat b -> SNat c ->
-      SNat (a * c) -> SNat (a + b) -> SNat (b * c) ->
-      ((a * c) + ((a + b) + (b * c))) :~: (((a * c) + (a + b)) + (b * c))
-    x5 _ _ _ x y z = gcastWith (plusAssoc x y z) Refl
+          x5 :: ((a * c) + ((a + b) + (b * c))) :~: (((a * c) + (a + b)) + (b * c))
+          x5 = gcastWith (plusAssoc xmz xpy ymz) Refl
 
-    x6 ::
-      SNat a -> SNat b -> SNat c ->
-      SNat (a * c) ->
-      (((a * c) + (a + b)) + (b * c)) :~: ((((a * c) + a) + b) + (b * c))
-    x6 a' b' _ x = gcastWith (plusAssoc x a' b') Refl
+          x6 :: (((a * c) + (a + b)) + (b * c)) :~: ((((a * c) + a) + b) + (b * c))
+          x6 = gcastWith (plusAssoc xmz x y) Refl
 
-    x7 ::
-      SNat a -> SNat b -> SNat c ->
-      SNat ((a * c) + a) -> SNat (b * c) ->
-      ((((a * c) + a) + b) + (b * c)) :~: (((a * c) + a) + (b + (b * c)))
-    x7 _ b' _ x y = gcastWith (plusAssoc x b' y) Refl
+          x7 :: ((((a * c) + a) + b) + (b * c)) :~: (((a * c) + a) + (b + (b * c)))
+          x7 = gcastWith (plusAssoc xmzpx y ymz) Refl
 
-    x8 ::
-      SNat a -> SNat b -> SNat c ->
-      SNat (b * c) ->
-      (((a * c) + a) + (b + (b * c))) :~: (((a * c) + a) + ((b * c) + b))
-    x8 _ b' _ x = gcastWith (plusComm b' x) Refl
+          x8 :: (((a * c) + a) + (b + (b * c))) :~: (((a * c) + a) + ((b * c) + b))
+          x8 = gcastWith (plusComm y ymz) Refl
 
-    x9 ::
-      SNat a -> SNat b -> SNat c ->
-      (((a * c) + a) + ((b * c) + b)) :~: ((a * S c) + (b * S c))
-    x9 _ _ _ = Refl
+          x9 :: (((a * c) + a) + ((b * c) + b)) :~: ((a * S c) + (b * S c))
+          x9 = Refl
 
-    amc = a !* c
-    bmc = b !* c
-    apb = a !+ b
-    amcpa = amc !+ a
+          xmz = x !* z
+          ymz = y !* z
+          xpy = x !+ y
+          xmzpx = xmz !+ x
 
-  in x1 a b k ==> x2 a b c ==> x3 a b c amc bmc apb ==> x4 a b c bmc apb ==> x5 a b c amc apb bmc ==> x6 a b c amc ==> x7 a b c amcpa bmc ==> x8 a b c bmc ==> x9 a b c
+  in proof a b c
 
--- Associativity
-mulAssoc :: SNat a -> SNat b -> SNat c -> ((a * b) * c) :~: (a * (b * c))
-mulAssoc a b SZ       = Refl
-mulAssoc a b k@(SS c) =
-  let
-    x1 :: SNat a -> SNat b -> SNat (S c) -> ((a * b) * S c) :~: (((a * b) * c) + (a * b))
-    x1 _ _ _ = Refl -- from (4)
-
-    x2 :: SNat a -> SNat b -> SNat c -> (((a * b) * c) + (a * b)) :~: ((a * (b * c)) + (a * b))
-    x2 a' b' c' = gcastWith (mulAssoc a' b' c') Refl
-
-    x3 :: SNat a -> SNat b -> SNat c ->
-          SNat (a * (b * c)) -> SNat (a * b) ->
-          ((a * (b * c)) + (a * b)) :~: ((a * b) + (a * (b * c)))
-    x3 _ _ _ x y = gcastWith (plusComm x y) Refl
-
-    x4 ::
-      SNat a -> SNat b -> SNat c ->
-      ((a * b) + (a * (b * c))) :~: ((b * a) + (a * (b * c)))
-    x4 a' b' _ = gcastWith (mulComm a' b') Refl
-
-    x5 ::
-      SNat a -> SNat b -> SNat c ->
-      SNat (b * c) ->
-      ((b * a) + (a * (b * c))) :~: ((b * a) + ((b * c) * a))
-    x5 a' _ _ x = gcastWith (mulComm a' x) Refl
-
-    x6 ::
-      SNat a -> SNat b -> SNat c ->
-      SNat (b * c) ->
-      ((b * a) + ((b * c) * a)) :~: ((b + (b * c)) * a)
-    x6 a' b' _ x = gcastWith (mulPlusDist b' x a') Refl
-
-    x7 ::
-      SNat a -> SNat b -> SNat c ->
-      SNat (b + (b * c)) ->
-      ((b + (b * c)) * a) :~: (a * (b + (b * c)))
-    x7 a' _ _ x = gcastWith (mulComm a' x) Refl
-
-    x8 ::
-      SNat a -> SNat b -> SNat c ->
-      SNat (b * c) ->
-      (a * (b + (b * c))) :~: (a * ((b * c) + b))
-    x8 _ b' _ x = gcastWith (plusComm b' x) Refl
-
-    x9 ::
-      SNat a -> SNat b -> SNat c ->
-      (a * ((b * c) + b)) :~: (a * (b * S c))
-    x9 _ _ _ = Refl
-
-    ambmc = a !* (b !* c)
-    amb = a !* b
-    bmc = b !* c
-    bpbmc = b !+ bmc
-
-  in x1 a b k ==> x2 a b c ==> x3 a b c ambmc amb ==> x4 a b c ==> x5 a b c bmc ==> x6 a b c bmc ==> x7 a b c bpbmc ==> x8 a b c bmc ==> x9 a b c
 
 mulComm :: SNat a -> SNat b -> (a * b) :~: (b * a)
 mulComm a SZ       = gcastWith (mulZeroPropL a) Refl
 mulComm a k@(SS b) =
   let
-    x1 :: SNat a -> SNat (S b) -> (a * S b) :~: ((a * b) + a)
-    x1 _ _ = Refl
+    proof :: forall a b c. SNat a -> SNat b -> (a * S b) :~: (S b * a)
+    proof x y = x1 ==> x2 ==> x3 ==> x4 ==> x5
+      where
+        x1 :: (a * S b) :~: ((a * b) + a)
+        x1 = Refl
 
-    x2 :: SNat a -> SNat b -> ((a * b) + a) :~: ((b * a) + a)
-    x2 a' b' = gcastWith (mulComm a' b') Refl
+        x2 :: ((a * b) + a) :~: ((b * a) + a)
+        x2 = gcastWith (mulComm x y) Refl
 
-    x3 :: SNat a -> SNat b -> ((b * a) + a) :~: ((b * a) + (I * a))
-    x3 a' _ = gcastWith (mulIdenL a') Refl
+        x3 :: ((b * a) + a) :~: ((b * a) + (I * a))
+        x3 = gcastWith (mulIdenL x) Refl
 
-    x4 :: SNat a -> SNat b -> ((b * a) + (I * a)) :~: ((b + I) * a)
-    x4 a' b' = gcastWith (mulPlusDist b' (SS SZ) a') Refl
+        x4 :: ((b * a) + (I * a)) :~: ((b + I) * a)
+        x4 = gcastWith (mulPlusDist y (SS SZ) x) Refl
 
-    x5 :: SNat a -> SNat b -> ((b + I) * a) :~: (S b * a)
-    x5 _ b' = Refl
+        x5 :: ((b + I) * a) :~: (S b * a)
+        x5 = Refl
 
-  in x1 a k ==> x2 a b ==> x3 a b ==> x4 a b ==> x5 a b
+  in proof a b
 
-mulAssoc' :: SNat a -> SNat b -> SNat c -> ((a * b) * c) :~: (a * (b * c))
-mulAssoc' a b SZ       = Refl
-mulAssoc' a b k@(SS c) =
+
+mulAssoc :: SNat a -> SNat b -> SNat c -> ((a * b) * c) :~: (a * (b * c))
+mulAssoc a b SZ       = Refl
+mulAssoc a b k@(SS c) =
   let proof :: forall a b c. SNat a -> SNat b -> SNat c -> ((a * b) * S c) :~: (a * (b * S c))
-      proof x y z = x1 ==>
-                    x2 ==>
-                    x3 ==>
-                    x4 ==>
-                    x5 ==>
-                    x6 ==>
-                    x7 ==>
-                    x8 ==>
-                    x9
+      proof x y z = x1 ==> x2 ==> x3 ==> x4 ==> x5 ==> x6 ==> x7 ==> x8 ==> x9
         where
           x1 :: ((a * b) * S c) :~: (((a * b) * c) + (a * b))
           x1 = Refl -- from (4)
 
           x2 :: (((a * b) * c) + (a * b)) :~: ((a * (b * c)) + (a * b))
-          x2 = gcastWith (mulAssoc' x y z) Refl
+          x2 = gcastWith (mulAssoc x y z) Refl
 
           x3 :: ((a * (b * c)) + (a * b)) :~: ((a * b) + (a * (b * c)))
           x3 = gcastWith (plusComm xmymz xmy) Refl
@@ -367,6 +253,7 @@ mulAssoc' a b k@(SS c) =
           ypymz = y !+ ymz
 
   in proof a b c
+
 --
 -- Use proofs
 --
@@ -390,8 +277,8 @@ instance (Show a) => Show (Vec n a) where
                       V0   -> ""
                       _    -> ", "
 
--- x = 1 :> 2 :> 3 :> 4 :> V0
--- y = 5 :> 6 :> 7 :> 8 :> 9 :> V0
+x = 1 :> 2 :> 3 :> 4 :> V0
+y = 5 :> 6 :> 7 :> 8 :> 9 :> V0
 
 append :: SNat n -> SNat m -> Vec n a -> Vec m a -> Vec (n + m) a
 append SZ m V0 ys = gcastWith (plusIdenL m) ys
